e657dd707c89682644a8d95537bcae220ac41cef
Microzenas <microzenas@gmail.com>
2022-10-22 23:58:34 -0700

    header file

---
+++ common/demo.cpp
@@ -0,0 +1,6 @@
+#include "demo.hpp"
+#include <iostream>
+using namespace std;
+void HelloWorld(string name){
+    cout<<"Hello World!!!!!"<<name<<endl;
+}
---
+++ common/demo.hpp
@@ -0,0 +1,6 @@
+
+#ifndef demo_hpp
+#define demo_hpp
+#include <iostream>
+void HelloWorld(std::string);
+#endif
