3617d1514425642985fdc3e333980d99323fba08
Microzenas <microzenas@gmail.com>
2022-10-24 21:58:32 -0700

    The project and coding designing

--- connect4/connect4.hpp
+++ connect4/connect4.hpp
@@ -7,15 +7,6 @@
 #include <iostream>
 #include <time.h>
 #include <string>
-class Game
-{
-public:
-    int getInTurn();
-    void dropPiece(int);
-    bool isWin();
-    std::string **getTable();
-protected:
-};
 
 class Connect4
 {
@@ -31,4 +22,5 @@ private:
     void _reset();
     std::string **_table;
 };
+
 #endif
