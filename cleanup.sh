16c6664d33d3ff5a89f364cd71db2e987f97ae01
Microzenas <microzenas@gmail.com>
2022-10-26 16:55:46 -0700

    cleanup file

--- cleanup.sh
+++ cleanup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 find ./ -name 'd.*' -type f -o -name 'o.*' -type f | xargs rm
 find ./ -name '*.d' -type f -o -name '*.o' -type f | xargs rm
+find ./ -name '*.d.*' -type f -o -name '*.o.*' -type f | xargs rm
